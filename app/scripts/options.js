1ecfb8cdf0cbeda5c4df027ea7191585c7f37d26
greatghoul <greatghoul@gmail.com>
2019-03-06 16:52:28 +0800

    事件监听并更新设置项

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,21 @@
+function updateRadioValue (evt) {
+  const name = $(evt.target).attr('name')
+  let value = $(`[name="${name}"]:checked`).val()
+  if (value in ['true', 'false']) {
+    value = JSON.parse(value)
+  }
+
+  app.setOptions({ [name]: value })
+}
+
+function updateRangeValue (evt) {
+  const $range = $(evt.target)
+  const name = $range.attr('name')
+  const value = $range.val()
+  app.setOptions({ [name]: parseInt(value) })
+  $range.next().html(`${value} 秒`)
+}
+
 function init (options) {
   $(`[name="translator"][value="${options.translator}"]`).prop('checked', true)
   $(`[name="pageInspect"][value="${options.pageInspect}"]`).prop('checked', true)
@@ -5,6 +23,9 @@ function init (options) {
   $(`[name="notifyMode"][value="${options.notifyMode}"]`).prop('checked', true)
   $('.notify-timeout').find('input').val(options.notifyTimeout)
   $('.notify-timeout').find('span').html(`${options.notifyTimeout} 秒`)
+
+  $(document).on('change', '[type=radio]', updateRadioValue)
+  $(document).on('change', '[type=range]', updateRangeValue)
 }
 
 app.getOptions().then(init)
