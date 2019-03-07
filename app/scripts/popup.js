235988f9fd6c01b3c97322a2ed250499a8cd405f
greatghoul <greatghoul@gmail.com>
2019-03-07 22:36:45 +0800

    点击切换查询服务

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,5 @@
+const TRANSLATORS = ['baidu', 'youdao', 'bing']
+
 function updateCheckboxValue (event) {
   const $checkbox = $(event.target)
   const name = $checkbox.attr('name')
@@ -5,10 +7,21 @@ function updateCheckboxValue (event) {
   app.setOptions({ [name]: value })
 }
 
+function setNextTranslator () {
+  const $translator = $(this)
+  const translator = $translator.attr('data-translator')
+  const index = TRANSLATORS.indexOf(translator) + 1
+  const nextTranslator = TRANSLATORS[index % TRANSLATORS.length]
+  $translator.attr('data-translator', nextTranslator)
+  app.setOptions({ translator: nextTranslator })
+}
+
 function init (options) {
+  $('.btn-translator').attr('data-translator', options.translator)
   $('[name="pageInspect"]').prop('checked', options.pageInspect)
   $('[name="linkInspect"]').prop('checked', options.linkInspect)
   $(document).on('change', ':checkbox', updateCheckboxValue)
+  $('.btn-translator').on('click', setNextTranslator)
 }
 
 app.getOptions().then(init)
